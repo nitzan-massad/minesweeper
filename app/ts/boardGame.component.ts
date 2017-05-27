b17c762cf45e1bd763644ec8aa35b45038e09a62
nitzan <nitzanmassad@gmail.com>
2017-05-27 17:52:42 +0300

    27.5.17

--- app/ts/boardGame.component.ts
+++ app/ts/boardGame.component.ts
@@ -1,9 +1,8 @@
 import {Component} from 'angular2/core';
 
-
 @Component({
-    selector: 'my-app',
+    selector: 'boardGame',
     templateUrl: 'app/ts/boardGame.component.html'
 })
 
-export class AppComponent {}
+export class boardGame {}
--- app/ts/main.ts
+++ app/ts/main.ts
@@ -1,4 +1,5 @@
 import {bootstrap}    from 'angular2/platform/browser';
 import {AppComponent} from './app.component';
+import {boardGame} from "./boardGame.component";
 
-bootstrap(AppComponent);
+bootstrap(boardGame);
